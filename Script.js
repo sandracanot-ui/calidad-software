c654679da22824c565a906ab3c94cd552c0f39a0
slcano <slcano09@misena.edu.co>
2025-11-03 20:24:09 -0500

    Primera versión del sitio Calidad de Software

---
+++ Script.js
@@ -0,0 +1,14 @@
+calidad_software/
+│
+├── index.html
+├── normas.html
+├── modelos.html
+├── estandares.html
+├── pruebas.html
+├── conclusiones.html
+├── recomendaciones.html
+├── manual.html
+├── funciones.html
+│
+├── style.css
+└── script.js
